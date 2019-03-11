125d0641a52daf5a068af527ab5c4ddd7e6fb0e5
Dipti V <dipti.vete@gmail.com>
2019-03-10 23:44:47 -0400

    angularjs assignment 1 solution

---
+++ angularjs_module_1/app.js
@@ -0,0 +1,42 @@
+(function () {
+    'use strict';
+    
+    angular.module('myFirstApp', [])
+    
+    .controller('MyFirstController', function ($scope) {
+      $scope.name = "Yaakov";
+      $scope.sayHello = function () {
+        return "Hello Coursera!";
+      };
+    });
+    
+    })();
+
+(function () {
+    'use strict';
+    angular.module('LunchCheck', [])
+
+    .controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.item_list = "";
+        $scope.message = "";
+ 
+        $scope.checkIfTooMuch = function () {
+            if (!$scope.item_list) {
+               $scope.message = "Please enter data first";
+            } else {
+                $scope.item_list = $scope.item_list.split(",");
+                if ($scope.item_list.length < 4) {
+                    console.log($scope.item_list.length);
+                    $scope.message = "Enjoy!";
+                } else {
+                    $scope.message = "Too much!";
+                }
+            }
+
+        };
+      }
+
+})();    
